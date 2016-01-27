c366f7312b5e33df060c0ab5c8663077d1d6d1e8
Sebastian Rakel <sebastian@devunit.eu>
2016-01-27 21:57:56 +0100

    Use networkmask and addentional statemetns from dhcp_config, not test data anymore

--- marmoset/dhcp/isc_dhcp_ldap_config.py
+++ marmoset/dhcp/isc_dhcp_ldap_config.py
@@ -10,10 +10,15 @@ class ISCDhcpLdapConfig:
         server = Server('localhost', get_info=ALL)
         conn = Connection(server, 'cn=root,dc=example,dc=com', 'secret', auto_bind=True)
 
+        dhcpStatements = ["fixed-address %s;" % self.dhcp_config.ip_address,
+                          "option subnet-mask %s;" % self.dhcp_config.networkmask,
+                          "option routers %s;" % self.dhcp_config.gateway]
+
+        for additional_statement in self.dhcp_config.additional_statements:
+            dhcpStatements.append("%s %s;" % (additional_statement, self.dhcp_config.additional_statements[additional_statement]))
+
         entry_attributes = {'dhcpHWAddress': "ethernet %s" % self.dhcp_config.mac,
-                            'dhcpStatements': ["fixed-address %s;" % self.dhcp_config.ip_address,
-                                               "option subnet-mask %s;" % '255.255.255.0',
-                                               "option routers %s;" % self.dhcp_config.gateway],
+                            'dhcpStatements': dhcpStatements,
                             'dhcpComments': "date=%s dhcp-hostname=%s" % (datetime.now().strftime("%Y%m%d_%H%M%S"), self.dhcp_config.dhcp_hostname)}
 
         conn.add("cn=%s,dc=example,dc=com" % self.dhcp_config.ip_address, 'dhcpHost', entry_attributes)
