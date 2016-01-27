fa0a09498e2550aeb7403ab9f77c2b846ecd872c
Sebastian Rakel <sebastian@devunit.eu>
2016-01-27 22:33:15 +0100

    Make __init__.py PEP8 conform

--- marmoset/__init__.py
+++ marmoset/__init__.py
@@ -1,6 +1,9 @@
-from . import config, cli, validation
+from marmoset import config
+from marmoset import cli
+from marmoset import validation
 
-def run(config_file = None):
+
+def run(config_file=None):
     cfg = config.load(config_file)
     cli.parse(cfg)
 
