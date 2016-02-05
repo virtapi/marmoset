5766b155bf6bc685fd7a01674a4269d70800b38c
Sebastian Rakel <sebastian@devunit.eu>
2016-02-05 01:59:10 +0100

    Added Methods to get LDAP DHCP Information
    * You can delete LDAP Entries by DN
    * You can get LDAP DN by IP and by MAC
    * You can get all objects of class "dhcpHost"

--- marmoset/dhcp/isc_dhcp_ldap_config.py
+++ marmoset/dhcp/isc_dhcp_ldap_config.py
@@ -1,25 +1,158 @@
-from ldap3 import Server, Connection, ALL
+from ldap3 import Server, Connection, ALL, SUBTREE, BASE, ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES
 from datetime import datetime
+from marmoset import config as config_reader
+
+import re
+
+config = config_reader.load()
 
 
 class ISCDhcpLdapConfig:
     def __init__(self, dhcp_config):
         self.dhcp_config = dhcp_config
 
+    @staticmethod
+    def __get_server_connection():
+        server = Server(config['DHCPConfig'].get('ldap_server'),
+                        port=int(config['DHCPConfig'].get('ldap_port')),
+                        get_info=ALL)
+
+        conn = Connection(server,
+                          config['DHCPConfig'].get('ldap_bind_dn'),
+                          config['DHCPConfig'].get('ldap_passwort'),
+                          auto_bind=True)
+
+        return conn
+
     def save(self):
-        server = Server('localhost', get_info=ALL)
-        conn = Connection(server, 'cn=root,dc=example,dc=com', 'secret', auto_bind=True)
+        conn = self.__get_server_connection()
 
         dhcpStatements = ["fixed-address %s;" % self.dhcp_config.ip_address,
                           "option subnet-mask %s;" % self.dhcp_config.networkmask,
                           "option routers %s;" % self.dhcp_config.gateway]
 
         for additional_statement in self.dhcp_config.additional_statements:
-            dhcpStatements.append("%s %s;" % (additional_statement, self.dhcp_config.additional_statements[additional_statement]))
+            dhcpStatements.append("%s %s;" % (additional_statement,
+                                              self.dhcp_config.additional_statements[additional_statement]))
 
         entry_attributes = {'dhcpHWAddress': "ethernet %s" % self.dhcp_config.mac,
                             'dhcpStatements': dhcpStatements,
-                            'dhcpComments': "date=%s dhcp-hostname=%s" % (datetime.now().strftime("%Y%m%d_%H%M%S"), self.dhcp_config.dhcp_hostname)}
+                            'dhcpComments': "date=%s dhcp-hostname=%s" % (datetime.now().strftime("%Y%m%d_%H%M%S"),
+                                                                          self.dhcp_config.dhcp_hostname)}
 
-        conn.add("cn=%s,dc=example,dc=com" % self.dhcp_config.ip_address, 'dhcpHost', entry_attributes)
+        conn.add("cn=%s,%s" % (self.dhcp_config.ip_address, config['DHCPConfig'].get('ldap_client_base_dn')),
+                 'dhcpHost',
+                 entry_attributes)
 
+    @staticmethod
+    def all():
+        conn = ISCDhcpLdapConfig.__get_server_connection()
+
+        entry_generator = conn.extend.standard.paged_search(search_base=config['DHCPConfig'].get('ldap_client_base_dn'),
+                                                            search_filter='(objectClass=dhcpHost)',
+                                                            search_scope=SUBTREE,
+                                                            attributes=['cn'],
+                                                            paged_size=5,
+                                                            generator=True)
+        result = []
+        for entry in entry_generator:
+            result.append(ISCDhcpLdapConfig.get_by_ip(entry['attributes']['cn'][0]))
+
+        return result
+
+    @staticmethod
+    def __get_dn_by_ipv4(ip_address):
+        conn = ISCDhcpLdapConfig.__get_server_connection()
+        conn.search(search_base=config['DHCPConfig'].get('ldap_client_base_dn'),
+                    search_filter='(cn=%s)' % ip_address,
+                    search_scope=SUBTREE,
+                    paged_size=5,
+                    attributes=[ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES])
+
+        entries = conn.response
+
+        if len(entries) == 0:
+            return None
+
+        return entries[0]['dn']
+
+    @staticmethod
+    def __get_dn_by_mac(mac_address):
+        conn = ISCDhcpLdapConfig.__get_server_connection()
+        conn.search(search_base=config['DHCPConfig'].get('ldap_client_base_dn'),
+                    search_filter='(dhcpHWAddress=ethernet %s)' % mac_address,
+                    search_scope=SUBTREE,
+                    paged_size=5,
+                    attributes=[ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES])
+
+        entries = conn.response
+
+        if len(entries) == 0:
+            return None
+
+        return entries[0]['dn']
+
+    @staticmethod
+    def __get_dhcp_config(dn):
+        from marmoset.dhcp import DhcpConfig
+
+        conn = ISCDhcpLdapConfig.__get_server_connection()
+        conn.search(search_base=dn,
+                    search_filter='(objectClass=dhcpHost)',
+                    search_scope=SUBTREE,
+                    paged_size=5,
+                    attributes=[ALL_ATTRIBUTES, ALL_OPERATIONAL_ATTRIBUTES])
+
+        entries = conn.response
+
+        if len(entries) == 0:
+            return None
+
+        mac_option = str(entries[0]['attributes']['dhcpHWAddress'])
+
+        regex_gateway = 'option routers\s+([0-9]+.[0-9]+.[0-9]+.[0-9]+)'
+        regex_networkmask = 'option subnet-mask\s+([0-9]+.[0-9]+.[0-9]+.[0-9]+)'
+
+        mac = re.search('(([0-9a-f]{2}:){5}[0-9a-f]{2})', mac_option).group(0)
+        ip = entries[0]['attributes']['cn'][0]
+
+        gateway = None
+        networkmask = None
+
+        for dhcpStatement in entries[0]['attributes']['dhcpStatements']:
+            if re.match(regex_gateway, dhcpStatement):
+                gateway = re.search(regex_gateway, dhcpStatement).group(1)
+
+            if re.match(regex_networkmask, dhcpStatement):
+                networkmask = re.search(regex_networkmask, dhcpStatement).group(1)
+
+        dhcp_config = DhcpConfig(mac, ip, gateway, networkmask)
+
+        additional_statements_str = config['DHCPConfig'].get('additional_statements')
+        additional_statements = additional_statements_str.split(',')
+
+        for ldap_additional_statement in entries[0]['attributes']['dhcpStatements']:
+            for additional_statement in additional_statements:
+                regex_additional_statement = '%s\s+(.*);' % additional_statement
+
+                if re.match(regex_additional_statement, ldap_additional_statement):
+                    value = re.search(regex_additional_statement, ldap_additional_statement).group(1)
+                    dhcp_config.add_additional_statement(additional_statement, value)
+
+        return dhcp_config
+
+    @staticmethod
+    def get_by_ip(ip_address):
+        return ISCDhcpLdapConfig.__get_dhcp_config(ISCDhcpLdapConfig.__get_dn_by_ipv4(ip_address))
+
+    @staticmethod
+    def get_by_mac(mac_address):
+        return ISCDhcpLdapConfig.__get_dhcp_config(ISCDhcpLdapConfig.__get_dn_by_mac(mac_address))
+
+    @staticmethod
+    def remove(ipv4):
+        dn = ISCDhcpLdapConfig.__get_dn_by_ipv4(ipv4)
+
+        conn = ISCDhcpLdapConfig.__get_server_connection()
+        conn.delete(dn)
+        conn.unbind()
