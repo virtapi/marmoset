677907e95b27fcb45358e32161238675d408de68
Sebastian Rakel <sebastian@devunit.eu>
2016-02-05 10:16:21 +0100

    Added correct implementation and checks of dhcp endpoint
    * /dhcp endoint have GET,POST
    * /dhcp/ipv4/<ipv4> endpoint have GET,PUT,DELETE
    * /dhcp/mac/<mac> endpoint have GET,PUT,DELETE
    * The gateway and networkmask arguments are not requires anymore

--- marmoset/webserver/dhcp.py
+++ marmoset/webserver/dhcp.py
@@ -4,26 +4,26 @@ from flask.ext.restful import Resource
 from flask.ext.restful import url_for
 from flask.ext.restful import abort
 from marmoset import dhcp
+from marmoset import config as config_reader
 
-parser = None
+config = config_reader.load()
 
+additional_statements_str = config['DHCPConfig'].get('additional_statements')
+additional_statements = additional_statements_str.split(',')
 
-def build_parameters(additional_statements):
-    global parser
+parser = reqparse.RequestParser()
+parser.add_argument('mac', type=str, required=True)
+parser.add_argument('ip_address', type=str, required=True)
+parser.add_argument('gateway', type=str, required=False, default=None)
+parser.add_argument('networkmask', type=str, required=False, default=None)
 
-    parser = reqparse.RequestParser()
-    parser.add_argument('mac', type=str, required=True)
-    parser.add_argument('ip_address', type=str, required=True)
-    parser.add_argument('gateway', type=str, required=False, default=None)
-    parser.add_argument('networkmask', type=str, required=False, default=None)
-
-    for additional_statement in additional_statements:
-        parser.add_argument(additional_statement, type=str, required=False)
+for additional_statement in additional_statements:
+    parser.add_argument(additional_statement, type=str, required=False)
 
 
 class DhcpCollection(Resource):
     def get(self):
-        return [vars(c) for c in dhcp.DhcpConfig]
+        return [vars(c) for c in dhcp.DhcpConfig.all()]
 
     def post(self):
         args = parser.parse_args()
@@ -31,65 +31,90 @@ class DhcpCollection(Resource):
         dhcp_config = dhcp.DhcpConfig(args.mac, args.ip_address, args.gateway, args.networkmask)
 
         for args_item in parser.args:
-            if not args_item.required and args_item.name in args and args[args_item.name] is not None:
+            if not args_item.required and args_item.name in args and args[args_item.name] is not None and args_item.name is not 'gateway' and args_item.name is not 'networkmask':
                 dhcp_config.add_additional_statement(args_item.name, args[args_item.name])
 
         dhcp_config.create_isc_ldap()
 
+        location = url_for('dhcpipv4object', _method='GET', ipv4=dhcp_config.ip_address)
+        return vars(dhcp_config), 201, {'Location': location}
+
 
 class DhcpIpv4Object(Resource):
     def get(self, ipv4):
-        dhcp_config = dhcp.DhcpConfig(ipv4=ipv4)
+        if not dhcp.DhcpConfig.exists_ipv4(ipv4):
+            return abort(404)
 
-        if dhcp_config.exists():
-            return vars(dhcp_config)
-        else:
-            abort(404)
+        dhcp_config = dhcp.DhcpConfig.get_by_ip(ipv4)
+
+        return vars(dhcp_config)
 
     def put(self, ipv4):
         args = parser.parse_args(request)
 
-        dhcp_config = dhcp.DhcpConfig(ipv4=ipv4)
+        if not dhcp.DhcpConfig.exists_ipv4(ipv4):
+            return abort(404)
 
-        dhcp_config.create()
+        dhcp_config = dhcp.DhcpConfig.get_by_ip(ipv4)
 
-        location = url_for('dhcpipv4object', _method='GET', ipv4=dhcp_config.ipv4)
+        dhcp_config.set_settings(False, args.mac, args.ip_address, args.gateway, args.networkmask)
+
+        for args_item in parser.args:
+            if not args_item.required and args_item.name in args and args[args_item.name] is not None and args_item.name is not 'gateway' and args_item.name is not 'networkmask':
+                dhcp_config.add_additional_statement(args_item.name, args[args_item.name])
+
+        dhcp_config.remove()
+
+        dhcp_config.create_isc_ldap()
+
+        location = url_for('dhcpipv4object', _method='GET', ipv4=dhcp_config.ip_address)
         return vars(dhcp_config), 201, {'Location': location}
 
     def delete(self, ipv4):
-        dhcp_config = dhcp.DhcpConfig(ipv4=ipv4)
+        if not dhcp.DhcpConfig.exists_ipv4(ipv4):
+            return abort(404)
 
-        if dhcp_config.exists():
-            dhcp_config.remove()
-            return '', 204
-        else:
-            abort(404)
+        dhcp_config = dhcp.DhcpConfig.get_by_ip(ipv4)
+        dhcp_config.remove()
+
+        return '', 204
 
 
 class DhcpMacObject(Resource):
     def get(self, mac):
-        dhcp_config = dhcp.DhcpConfig(mac=mac)
+        dhcp_config = dhcp.DhcpConfig.get_by_mac(mac)
 
-        if dhcp_config.exists():
-            return vars(dhcp_config)
-        else:
-            abort(404)
+        if not dhcp.DhcpConfig.exists_mac(mac):
+            return abort(404)
+
+        return vars(dhcp_config)
 
     def put(self, mac):
         args = parser.parse_args(request)
 
-        dhcp_config = dhcp.DhcpConfig(mac)
+        if not dhcp.DhcpConfig.exists_mac(mac):
+            return abort(404)
 
-        dhcp_config.create()
+        dhcp_config = dhcp.DhcpConfig.get_by_mac(mac)
+
+        dhcp_config.set_settings(False, args.mac, args.ip_address, args.gateway, args.networkmask)
+
+        for args_item in parser.args:
+            if not args_item.required and args_item.name in args and args[args_item.name] is not None and args_item.name is not 'gateway' and args_item.name is not 'networkmask':
+                dhcp_config.add_additional_statement(args_item.name, args[args_item.name])
+
+        dhcp_config.remove()
+
+        dhcp_config.create_isc_ldap()
 
         location = url_for('dhcpmacobject', _method='GET', mac=dhcp_config.mac)
         return vars(dhcp_config), 201, {'Location': location}
 
     def delete(self, mac):
-        dhcp_config = dhcp.DhcpConfig(mac)
+        if not dhcp.DhcpConfig.exists_mac(mac):
+            return abort(404)
 
-        if dhcp_config.exists():
-            dhcp_config.remove()
-            return '', 204
-        else:
-            abort(404)
+        dhcp_config = dhcp.DhcpConfig.get_by_mac(mac)
+        dhcp_config.remove()
+
+        return '', 204
