088063c9004913728799f21b1a90f912f1a55bb2
pbyatshon <oliver.schmitz@heg.com>
2016-03-14 00:02:18 +0100

    added cli functions

---
+++ marmoset/installstatus/__ini__.py
@@ -0,0 +1,46 @@
+from .installstatus import InstallStatus
+from marmoset import validation
+from uuid import UUID
+
+
+def get_history(args):
+    if is_uuid(args.uuid):
+        uuid = str(UUID(args.uuid))
+        install_status = InstallStatus(uuid)
+        history = install_status.get_history()
+        if history:
+            for status in history:
+                print('%s: %s' % (status['date'], status['status']))
+        else:
+            print('No status updates for %s found.' % uuid)
+
+
+def get_latest(args):
+    if is_uuid(args.uuid):
+        uuid = str(UUID(args.uuid))
+        install_status = InstallStatus(uuid)
+        latest = install_status.get_latest_status()
+        if latest is not None:
+            print('%s: %s' % (latest['date'], latest['status']))
+        else:
+            print('No status updates for %s found.' % uuid)
+
+
+def get_stats(args):
+    if is_uuid(args.uuid):
+        uuid = str(UUID(args.uuid))
+        install_status = InstallStatus(uuid)
+        stats = install_status.get_stats()
+        print_stats_order = ['uuid', 'start_date', 'end_date', 'duration',
+                             'status_updates', 'latest_status',
+                             'latest_status_age']
+        for stat in print_stats_order:
+            print('%s: %s' % (stat, stats[stat]))
+
+
+def is_uuid(uuid):
+    if validation.is_uuid(uuid):
+        return True
+    else:
+        print('Invalid uuid %s.' % uuid)
+        return False
