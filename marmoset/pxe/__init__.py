2273884360041012628a79ebb89d2729919832d2
Tim Meusel <tim@bastelfreak.de>
2016-04-27 00:37:35 +0200

    fix pep257 in marmoset/pxe/__init__.py

--- marmoset/pxe/__init__.py
+++ marmoset/pxe/__init__.py
@@ -1,8 +1,10 @@
+"""Base file for PXE interaction"""
 from .client_config import ClientConfig
 from .label import Label
 
 
 def create(args):
+    """creates a new PXE entry"""
     pxe_client = ClientConfig(args.ip_address, args.password, args.script)
     used_options = pxe_client.create(Label.find(args.label))
 
@@ -11,12 +13,14 @@ def create(args):
     print(msg % (pxe_client.file_path(), " ".join(used_options)))
 
 
-def list(args):
+def list():
+    """List all PXE entries"""
     for pxe_client in ClientConfig.all():
         print('%s: %s' % (pxe_client.ip_address, pxe_client.label))
 
 
 def remove(args):
+    """remove a specific PXE entry"""
     pxe_client = ClientConfig(args.ip_address)
     if pxe_client.remove():
         print('Removed', pxe_client.file_path())
