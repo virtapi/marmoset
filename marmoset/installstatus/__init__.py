6c0f1aacf1765554f6d13f34cbb424202e39fb4c
pbyatshon <oliver.schmitz@heg.com>
2016-04-29 17:59:30 +0200

    extend cli methods to match extended table schema/installstatus methods

--- marmoset/installstatus/__init__.py
+++ marmoset/installstatus/__init__.py
@@ -1,50 +1,49 @@
+"""Initial file for installstatus"""
 from .installstatus import InstallStatus
 from marmoset import validation
 from uuid import UUID
 
 
 def get_history(args):
-    """
-    prints status update history related to installimage uuid
-
-    """
+    """prints status update history related to installimage uuid"""
     if is_uuid(args.uuid):
         uuid = str(UUID(args.uuid))
         install_status = InstallStatus(uuid)
         history = install_status.get_history()
         if history:
             for status in history:
-                print('%s: %s' % (status['date'], status['status']))
+                print('%s %s/%s %s %s' %
+                      (status['date'], status['current_step'],
+                       status['total_steps'], status['status_code'],
+                       status['step_description']))
         else:
             print('No status updates for %s found.' % uuid)
 
 
 def get_latest(args):
-    """
-    prints latest status update related to installimage uuid
-
-    """
+    """prints latest status update related to installimage uuid"""
     if is_uuid(args.uuid):
         uuid = str(UUID(args.uuid))
         install_status = InstallStatus(uuid)
         latest = install_status.get_latest_status()
         if latest is not None:
-            print('%s: %s' % (latest['date'], latest['status']))
+            print('%s %s/%s %s %s' %
+                  (latest['date'], latest['current_step'],
+                   latest['total_steps'], latest['status_code'],
+                   latest['step_description']))
         else:
             print('No status updates for %s found.' % uuid)
 
 
 def get_stats(args):
-    """
-    prints some stats related to installimage uuid
-
-    """
+    """prints some stats related to installimage uuid"""
     if is_uuid(args.uuid):
         uuid = str(UUID(args.uuid))
         install_status = InstallStatus(uuid)
         stats = install_status.get_stats()
-        print_stats_order = ['uuid', 'start_date', 'end_date', 'duration',
-                             'status_updates', 'latest_status',
+        print_stats_order = ['uuid', 'start_date', 'end_date',
+                             'installation_duration',
+                             'status_updates', 'latest_status_code',
                              'latest_status_age']
         for stat in print_stats_order:
             print('%s: %s' % (stat, stats[stat]))
