c51db87b48e7c4920ecf1c8117fc89276ea2d633
Tim Meusel <tim@bastelfreak.de>
2016-04-30 15:42:41 +0200

    add missing docstrings in req_argument_parser.py

--- marmoset/installimage/req_argument_parser.py
+++ marmoset/installimage/req_argument_parser.py
@@ -1,3 +1,9 @@
+"""Simple module to get access to data from an http request"""
+
+
 class ReqArgumentParser(object):
+    """Simple class to get access to data from an http request"""
+
     def parse_args(self, req):
+        """Method to parse arguments from a request"""
         return req.form
