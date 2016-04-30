58d4cd311e94b5e2bea184c138c942ae89b3bfb2
Tim Meusel <tim@bastelfreak.de>
2016-04-30 15:35:38 +0200

    add missing docstrings in pxe/exceptions.py

--- marmoset/pxe/exceptions.py
+++ marmoset/pxe/exceptions.py
@@ -1,8 +1,14 @@
+"""Module to deal with exceptions in the PXE part"""
+
+
 class Error(Exception):
+    """Class to deal with it"""
 
     def __init__(self, message):
         self.message = message
 
 
 class InputError(Error):
+    """Dummy class"""
+
     pass
